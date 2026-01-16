2862d14eef22be906767a0084ca855b7142df893
RASHI CHANDEL <165982606+Chandelrashi@users.noreply.github.com>
2026-01-16 13:52:47 +0000

    streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -159,4 +159,9 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as e:
+        st.error("Application failed to start.")
+        st.exception(e)
+

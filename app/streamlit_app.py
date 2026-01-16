9820b355f8bde093dba9207ac50db5f05bbdbf83
RASHI CHANDEL <165982606+Chandelrashi@users.noreply.github.com>
2026-01-16 12:30:41 +0000

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,3 +1,10 @@
+import sys
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+sys.path.append(str(PROJECT_ROOT))
+
+
 from __future__ import annotations
 
 from pathlib import Path
